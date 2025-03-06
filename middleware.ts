a5c023e3d8123aaa6cb94091d0c5e2bc12fcb8cb
Conner Kennedy <connerkennedy32@gmail.com>
2025-03-06 14:43:04 -0700

    PWA V.1

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 // const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhook(.*)'])
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api(.*)', '/manifest.webmanifest'])
 
 export default clerkMiddleware((auth, request) => {
     if (!isPublicRoute(request)) {
