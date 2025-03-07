2478998b09cc540f6578487e38d60a871d88e8fb
Conner Kennedy <connerkennedy32@gmail.com>
2025-03-06 18:32:59 -0700

    bump

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,17 +2,23 @@ import type { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: 'Next.js PWA',
-        short_name: 'NextPWA',
-        description: 'A Progressive Web App built with Next.js',
+        name: 'World Builder',
+        short_name: 'World Builder',
+        description: 'Build your world here!',
         start_url: '/',
         display: 'standalone',
+        orientation: 'portrait',
         background_color: '#ffffff',
         theme_color: '#000000',
         icons: [
             {
                 src: '/192x192.png',
                 sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/384x384.png',
+                sizes: '384x384',
                 type: 'image/png',
             },
             {
